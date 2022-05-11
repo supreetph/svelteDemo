0c6b70d510a01fa5a565184349d0fc338580a32b
Supreet Prakash Hadagali <shadagali@bhg-inc.com>
2022-05-11 19:24:03 +0530

    added forms

---
+++ src/routes/AddPerson.svelte
@@ -0,0 +1,16 @@
+<script>    
+let name
+let age
+let city
+
+const AddPerson=()=>{
+    console.log(name,age,city);
+}
+</script>
+
+<form action="" on:submit|preventDefault={AddPerson}>
+    Name: <input type="text" name="name" id="" bind:value={name}/><br/>
+    Age:<input type="text" name="age" id="" bind:value={age}/><br/>
+    City:   <input type="text" name="name" id="" bind:value={city}/><br/>
+        <button>Submit</button>
+</form>
